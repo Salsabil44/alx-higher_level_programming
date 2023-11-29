853d21573bda7940f86a9deab7651f1ece6d05f7
Salsabil44 <achisalsabil556@gmail.com>
2023-11-29 06:46:55 -0800

    Python

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -2,8 +2,4 @@
 output = ""
 for ascii_value in range(97, 123):
     output += chr(ascii_value)
-<<<<<<< HEAD
-print(output, end="")
-=======
 print("{}".format(output), end="")
->>>>>>> 82a5c57ce8934abc61b1f56f16283f8eb0fa8fb5
