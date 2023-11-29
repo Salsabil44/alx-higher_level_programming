5f182a83515b8bc1e08ff5f71841b2c03797a622
Salsabil44 <138724765+Salsabil44@users.noreply.github.com>
2023-11-29 15:35:11 +0100

    Update 2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-for f in range(97, 123):
-    print(chr(f).format(), end="")
+output = ""
+for ascii_value in range(97, 123):
+    output += chr(ascii_value)
+print(output, end="")
