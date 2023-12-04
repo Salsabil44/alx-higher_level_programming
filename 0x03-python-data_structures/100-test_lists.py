650fe6ced5bde133781bd777a1cbe7a438a6dd2a
Salsabil44 <achisalsabil556@gmail.com>
2023-12-04 15:22:32 -0800

    Python

--- 0x03-python-data_structures/100-test_lists.py
+++ 0x03-python-data_structures/100-test_lists.py
@@ -1,18 +1,23 @@
-#!/usr/bin/python3
 #include <Python.h>
-#include <stdio.h>
 
-void print_python_list_info(PyObject *p) {
-    Py_ssize_t size = PyList_Size(p);
-    Py_ssize_t allocated = ((PyListObject *) p)->allocated;
-    
-    printf("[*] Size of the Python List = %ld\n", size);
-    printf("[*] Allocated = %ld\n", allocated);
-    
-    for (Py_ssize_t i = 0; i < size; i++) {
-        PyObject *item = PyList_GetItem(p, i);
-        const char *itemType = Py_TYPE(item)->tp_name;
-        
-        printf("Element %ld: %s\n", i, itemType);
+/**
+*print_python_list_info - Prints basic info about Python lists.
+*@p: A PyObject list.
+*/
+void print_python_list_info(PyObject *p)
+{
+        int size, alloc, i;
+        PyObject *Obj;
+        size = Py_SIZE(p);
+        alloc = ((PyListObject *)p)->allocated;
+
+        print("[*] size of the Python List = %d/n", size);
+        print("[*] Allocated = %d/n", alloc);
+
+        for (i = 0; i < size; i++)
+        {
+                print("Element %d: ", i);
+                obj = PyList_GetItem(p, i);
+                print("%s/n", Py_TYPE(obj)->tp_name);
     }
 }
