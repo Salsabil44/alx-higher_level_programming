69821d3d76284db8b1753b74c871cfcc22fe737f
Salsabil44 <achisalsabil556@gmail.com>
2023-12-19 14:58:35 -0800

    p

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""  square module"""
+"""Square module"""
 
 class Square:
-    """ defines a square"""
+    """Defines a square"""
 
     def __init__(self, size=0):
         """initialize square
