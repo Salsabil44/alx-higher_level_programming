0b883d3f6c3d560578d2a9ed9415821588b1d83f
Salsabil44 <achisalsabil556@gmail.com>
2024-01-08 01:27:46 -0800

    PY

--- 0x07-python-test_driven_development/tests/102-python.c
+++ 0x07-python-test_driven_development/tests/102-python.c
@@ -11,25 +11,24 @@
 
 void print_python_string(PyObject *p)
 {
+    PyObject *repr = PyObject_Repr(p);  // Use repr to get the string representation
+    PyObject *str = PyUnicode_AsEncodedString(p, "utf-8", "~E~");
 
-	PyObject *str, *repr;
+    printf("[.] string object info\n");
 
-	(void)repr;
-	printf("[.] string object info\n");
+    if (strcmp(p->ob_type->tp_name, "str") != 0)
+    {
+        printf("  [ERROR] Invalid String Object\n");
+        return;
+    }
 
-	if (strcmp(p->ob_type->tp_name, "str"))
-	{
-		printf("  [ERROR] Invalid String Object\n");
-		return;
-	}
+    if (PyUnicode_IS_COMPACT_ASCII(p))
+        printf("  type: compact ascii\n");
+    else
+        printf("  type: compact unicode object\n");
 
-	if (PyUnicode_IS_COMPACT_ASCII(p))
-		printf("  type: compact ascii\n");
-	else
-		printf("  type: compact unicode object\n");
-
-	repr = PyObject_Repr(p);
-	str = PyUnicode_AsEncodedString(p, "utf-8", "~E~");
-	printf("  length: %ld\n", PyUnicode_GET_SIZE(p));
-	printf("  value: %s\n", PyBytes_AsString(str));
+    printf("  length: %ld\n", PyUnicode_GET_SIZE(p));
+    printf("  value: %s\n", PyBytes_AsString(str));
+    printf("  repr: %s\n", PyBytes_AsString(repr));  // Print the representation
 }
+
