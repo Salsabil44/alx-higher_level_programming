156124e50465da9d87ae25b4a0d35ba11ff4b095
Salsabil44 <achisalsabil556@gmail.com>
2024-01-10 21:45:46 +0000

    json

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
-""" @author : sally
+"""Reads from standard input and computes metrics.
+
+After every ten lines or the input of a keyboard interruption (CTRL + C),
+prints the following statistics:
+    - Total file size up to that point.
+    - Count of read status codes up to that point.
+@author : sally
 """
 
 
@@ -13,7 +19,6 @@ def print_stats(size, status_codes):
     print("File size: {}".format(size))
     for key in sorted(status_codes):
         print("{}: {}".format(key, status_codes[key]))
-
 
 if __name__ == "__main__":
     import sys
