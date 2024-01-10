250a30af34e6c4547e1f1c20e2df9c88848c0826
Salsabil44 <achisalsabil556@gmail.com>
2024-01-10 21:55:50 +0000

    json

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -5,7 +5,6 @@ After every ten lines or the input of a keyboard interruption (CTRL + C),
 prints the following statistics:
     - Total file size up to that point.
     - Count of read status codes up to that point.
-@author : sally
 """
 
 
@@ -19,6 +18,7 @@ def print_stats(size, status_codes):
     print("File size: {}".format(size))
     for key in sorted(status_codes):
         print("{}: {}".format(key, status_codes[key]))
+
 
 if __name__ == "__main__":
     import sys
@@ -57,3 +57,4 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         print_stats(size, status_codes)
         raise
+"""@author : sally"""
