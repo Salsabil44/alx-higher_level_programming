4ae72933f028d37baf66adc96b8603f3d465fd3f
Salsabil44 <achisalsabil556@gmail.com>
2024-03-11 16:16:32 -0700

    js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-// prints 3 lines: (like 1-multi_languages.js) but by sally
 const langs = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 for (let i = 0; i < 3; i++) {
   console.log(langs[i]);
